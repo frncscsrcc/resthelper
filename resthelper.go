3f31aca0cf5c9e8ae9a51b1aaf6e5383e6196c8e
Francesco Sirocco <frncscsrcc@gmail.com>
2019-10-15 06:55:44 +0200

    WIP refactoring

--- resthelper.go
+++ resthelper.go
@@ -1,8 +1,10 @@
 package resthelper
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"log"
 	"math/rand"
 	"net/http"
 	"time"
@@ -11,15 +13,15 @@ import (
 var seededRand *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 type ErrorResponse struct {
-	Code    int
-	Message string
+	Error     bool
+	ErrorCode int
+	Message   string
 }
-
 
 func SendError(w http.ResponseWriter, code int, message string) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
-	json, err := toJSON(ErrorResponse{code, message})
+	json, err := toJSON(ErrorResponse{true, code, message})
 	if err != nil {
 		SendError(w, 500, err.Error())
 		return
@@ -53,4 +55,52 @@ func GetNewToken(length int) string {
 		b[i] = charset[seededRand.Intn(len(charset))]
 	}
 	return string(b)
+}
+
+func GetSessionID(r *http.Request) string {
+	// Search in the context
+	sessionID, ok := r.Context().Value("sessionID").(string)
+	if ok && sessionID != "" {
+		return sessionID
+	}
+	// Search in URL
+	sessionIDSlice, ok := r.URL.Query()["sessionID"]
+	if ok != true {
+		return ""
+	}
+	return sessionIDSlice[0]
+	// Search in body
+	// TODO
+}
+
+func GetToken(r *http.Request) string {
+	// Search in the context
+	token, ok := r.Context().Value("token").(string)
+	if ok && token != "" {
+		return token
+	}
+	// Search in URL
+	tokenSlice, ok := r.URL.Query()["token"]
+	if ok != true {
+		return ""
+	}
+	return tokenSlice[0]
+	// Search in body
+	// TODO
+}
+
+func LogRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		sessionID := r.Context().Value("sessionID")
+		log.Println(r.Method, "-", sessionID, "-", r.RequestURI)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func AddSessionID(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		sessionID := GetSessionID(r)
+		ctx := context.WithValue(r.Context(), "sessionID", sessionID)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
 }
